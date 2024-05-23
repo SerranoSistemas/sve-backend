4e5f1dae80f7e64886e0284d27563b365a24efdf
BrunoKappi <brunokappidematos2@gmail.com>
2024-05-23 16:13:43 -0300

    Added Planta

--- index.ts
+++ index.ts
@@ -1,15 +1,2 @@
-import express, { Request, Response } from "express";
-
-import PlantaRouter from "./Routes/Planta";
-
-const APP = express();
-
-APP.get("/", (req: Request, res: Response) =>
-  res.status(200).json({ message: "Bem vindo ao Sistema de Medição" })
-);
-
-APP.use("/planta", PlantaRouter);
-
-APP.listen(3000, () => console.log("Server is running on port 3000"));
-
-export default APP;
+import app from "./server";
+export default app;
