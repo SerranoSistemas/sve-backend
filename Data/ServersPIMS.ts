e84b8c72812caf0f5ceff0c57a33c8b7b9b1028b
BrunoKappi <brunokappidematos2@gmail.com>
2024-07-05 09:56:47 -0300

    Adicionado /Servers

--- Data/ServersPIMS.ts
+++ Data/ServersPIMS.ts
@@ -1,39 +1,39 @@
 import moment from "moment";
 import { DefaultClass, ServersPIMS } from "./Types";
 
-export const PIMSServers: ServersPIMS[] = [
+export const PIMSServers: DefaultClass[] = [
   {
     identificador: "PIMS1",
-    title: "Eteno Verde",
-    odbc: "INFOEV101",
+    codigo: "Eteno Verde",
+    descricao: "INFOEV101",
     incluidoEm: moment(new Date()).unix(),
     alteradoEm: moment(new Date()).unix(),
   },
   {
     identificador: "PIMS2",
-    title: "Olefinas 1",
-    odbc: "OLEFI1-IP21",
+    codigo: "Olefinas 1",
+    descricao: "OLEFI1-IP21",
     incluidoEm: moment(new Date()).unix(),
     alteradoEm: moment(new Date()).unix(),
   },
   {
     identificador: "PIMS3",
-    title: "Olefinas 2",
-    odbc: "OLEFI2-IP21",
+    codigo: "Olefinas 2",
+    descricao: "OLEFI2-IP21",
     incluidoEm: moment(new Date()).unix(),
     alteradoEm: moment(new Date()).unix(),
   },
   {
     identificador: "PIMS4",
-    title: "Utilidades",
-    odbc: "UTIL-IP21",
+    codigo: "Utilidades",
+    descricao: "UTIL-IP21",
     incluidoEm: moment(new Date()).unix(),
     alteradoEm: moment(new Date()).unix(),
   },
   {
     identificador: "PIMS5",
-    title: "Conexão UNIB RS",
-    odbc: "WTRI83",
+    codigo: "Conexão UNIB RS",
+    descricao: "WTRI83",
     incluidoEm: moment(new Date()).unix(),
     alteradoEm: moment(new Date()).unix(),
   },
@@ -41,7 +41,7 @@ export const PIMSServers: ServersPIMS[] = [
 
 export const PIMSServersDropdown = PIMSServers.map((PIMS) => {
   return {
-    label: PIMS.odbc,
+    label: PIMS.descricao,
     value: PIMS.identificador,
   };
 });
