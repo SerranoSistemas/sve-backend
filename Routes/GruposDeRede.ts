365b1d64c9eab858a96b7f8904188bc4b8cdcddb
BrunoKappi <brunokappidematos2@gmail.com>
2024-07-19 10:53:55 -0300

    Adicionado Endpoint de Grupos de Rede

---
+++ Routes/GruposDeRede.ts
@@ -0,0 +1,52 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { GetElementByID, Middleware } from "../Lib/Utils";
+import { ResponseType } from "../Data/Types";
+import { GruposDeRede, GruposDeRedeDropdown } from "../Data/GruposDeRede";
+
+const Router = express.Router();
+const HTTP_GET = Router.get.bind(Router);
+const HTTP_POST = Router.post.bind(Router);
+const HTTP_DELETE = Router.delete.bind(Router);
+const HTTP_PUT = Router.put.bind(Router);
+
+//Apply JSON parse
+Router.use(express.json());
+//Apply Middleware for Delay and Error simulation
+Router.use(Middleware);
+// Use o middleware CORS
+Router.use(cors());
+
+HTTP_GET("/", (req: Request, res: Response) => res.status(200).json({ message: "Módulo de Grupos de Rede" }));
+
+HTTP_GET("/get-list", (REQ: Request, RES: Response) => {
+  const Response: ResponseType = {
+    data: GruposDeRedeDropdown,
+    success: true,
+    message: "Dados processados com sucesso",
+  };
+
+  return RES.status(200).json(Response);
+});
+
+HTTP_GET("/:id", (REQ: Request, RES: Response) => {
+  const data = GetElementByID(GruposDeRede, REQ.params.id, "GruposDeRede");
+
+  RES.status(200).json({
+    data: [...data?.permissoes] || [],
+    success: data ? true : false,
+    message: data ? "Dados processados com sucesso" : "Dado não encontrado",
+  });
+});
+
+HTTP_PUT("/:id", (REQ: Request, RES: Response) => {
+  const Response: ResponseType = {
+    data: REQ.body,
+    success: true,
+    message: `Editando Permissões do Grupo de Rede com ID: ${REQ.params.id}`,
+  };
+
+  RES.status(200).json(Response);
+});
+
+export { Router };
--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ import { Router as Router_Medicoes } from "./Routes/Medicoes";
 import { Router as Router_UnidadesDeMedida } from "./Routes/UnidadesDeMedida";
 import { Router as Router_Produto } from "./Routes/Produto";
 import { Router as SistemaRoutes } from "./Routes/Sistema";
-import { Router as GruposDeRedeRoutes } from "./Routes/grupoderede";
+import { Router as GruposDeRedeRoutes } from "./Routes/GruposDeRede";
 import { Router as ConfiguracaoRoutes } from "./Routes/Configuracao";
 
 const APP = express();
@@ -24,8 +24,8 @@ APP.use("/produto", Router_Produto);
 APP.use("/sistema", SistemaRoutes);
 APP.use("/estacaoMedicao", EstacaoMedicaoRouter);
 APP.use("/locales", LocalesRouter);
- APP.use("/grupoderede", GruposDeRedeRoutes);
-// APP.use("/configuracao", ConfiguracaoRoutes);
+APP.use("/grupoderede", GruposDeRedeRoutes);
+APP.use("/configuracao", ConfiguracaoRoutes);
 
 APP.listen(3000, () => console.log("Server is running on port 3000"));
 
