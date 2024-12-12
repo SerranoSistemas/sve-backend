8ff09a475244c36620f247fcc0d6773dcb5e6687
BrunoKappi <brunokappidematos2@gmail.com>
2024-12-12 10:44:30 -0300

    Relatorio de Entregas

--- Data/RelatorioEntregas.ts
+++ Data/RelatorioEntregas.ts
@@ -49,11 +49,10 @@ export const GerarRelatorioEntregas = (
   const Dados = ArrayDeDatas.map((Dia) => {
     return {
       data: Dia,
-      dados: [
-        Agrupadores.map(() => {
-          return [999.999];
-        }),
-      ],
+      dados: Agrupadores.map((_, index) => {
+        const i = index + 1;
+        return [i, i * 10, i * 100, i * 1000, i * 1000, i * 1000, i * 1000, i * 1000, i * 1000, i * 1000, i * 1000];
+      }),
     };
   });
 
--- Routes/Sistema.ts
+++ Routes/Sistema.ts
@@ -36,16 +36,16 @@ Router.get("/inicio", (REQ: Request, RES: Response) => {
       es: "Secundario",
     },
     permissoes: [
-      //TODAS AS PERMISSÔES CONCEDIDAS
-      //MODULO DE SAP
-      // { chave: "1", valor: ESCRITA },
+      // //TODAS AS PERMISSÔES CONCEDIDAS
+      // //MODULO DE SAP
+      // // { chave: "1", valor: ESCRITA },
       { chave: "1.1", valor: ESCRITA },
       { chave: "1.2", valor: ESCRITA },
       { chave: "1.3", valor: ESCRITA },
       { chave: "1.4", valor: ESCRITA },
 
-      //MODULO DE ADM
-      // { chave: "2", valor: ESCRITA },
+      // //MODULO DE ADM
+      // // { chave: "2", valor: ESCRITA },
       { chave: "2.1", valor: ESCRITA },
       { chave: "2.2", valor: ESCRITA },
       { chave: "2.3", valor: ESCRITA },
@@ -53,15 +53,15 @@ Router.get("/inicio", (REQ: Request, RES: Response) => {
       { chave: "2.5", valor: ESCRITA },
       { chave: "2.6", valor: ESCRITA },
 
-      //MODULO DE MOVIMENTACAO
-      // { chave: "3", valor: ESCRITA },
-      { chave: "3.1", valor: ESCRITA }, 
+      // //MODULO DE MOVIMENTACAO
+      // // { chave: "3", valor: ESCRITA },
+      { chave: "3.1", valor: ESCRITA },
       { chave: "3.2", valor: ESCRITA },
       { chave: "3.3", valor: ESCRITA },
       { chave: "3.4", valor: ESCRITA },
 
-      //MODULO DE CADASTRO
-      // { chave: "4", valor: ESCRITA },
+      // //MODULO DE CADASTRO
+      // // { chave: "4", valor: ESCRITA },
       { chave: "4.1", valor: ESCRITA },
       { chave: "4.2", valor: ESCRITA },
       { chave: "4.3", valor: ESCRITA },
@@ -71,8 +71,8 @@ Router.get("/inicio", (REQ: Request, RES: Response) => {
       { chave: "4.7", valor: ESCRITA },
       { chave: "4.8", valor: ESCRITA },
 
-      //MODULO DE RELATORIO
-      // { chave: "5", valor: ESCRITA },
+      // //MODULO DE RELATORIO
+      // // { chave: "5", valor: ESCRITA },
       { chave: "5.1", valor: ESCRITA },
       { chave: "5.2", valor: ESCRITA },
       { chave: "5.3", valor: ESCRITA },
